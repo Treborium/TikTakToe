61ea6ad9a0099350848260537ca032ade47e7b9c
Robert Fuchs <robertfuchs97@gmail.com>
2018-09-20 10:32:54 +0200

    make sure on occupied field nothing can be placed and place the symbol in the center of the clicked field.

--- Python/window.py
+++ Python/window.py
@@ -16,7 +16,11 @@ class TikTakToe(arcade.Window):
         self.xo_sprite_list = arcade.SpriteList()
         self.X_FILENAME = 'TikTakToe-X.png'
         self.O_FILENAME = 'TikTakToe-O.png'
+        self.GRID_SIZE_X = 3
+        self.GRID_SIZE_Y = 3
         self.x_turn = True
+
+        self.__init_board()
 
         self.game_board = arcade.Sprite(
             self.__getResourcePath('GameBoard.png'), 1.0)
@@ -27,19 +31,33 @@ class TikTakToe(arcade.Window):
         self.game_board.draw()
         self.xo_sprite_list.draw()
 
-    def on_mouse_press(self, x: float, y: float, button: int, modifiers: int) -> None:
+    def on_mouse_press(self, x: float, y: float,
+                       button: int, modifiers: int) -> None:
         self.__evaluate_turn(x, y)
 
     def update(self, delta_time: float):
         pass
 
     def __evaluate_turn(self, x: float, y: float) -> None:
+        x_index = int(x / (self.width / self.GRID_SIZE_X))
+        y_index = int(y / (self.height / self.GRID_SIZE_Y))
+
+        if self.board[x_index][y_index] != 0:
+            return
+
         if self.x_turn:
             sprite = self.__load_sprite(self.X_FILENAME)
+            self.board[x_index][y_index] = 'X'
         else:
             sprite = self.__load_sprite(self.O_FILENAME)
+            self.board[x_index][y_index] = 'O'
 
-        sprite.set_position(x, y)
+        x_position = ((self.width / self.GRID_SIZE_X) * x_index) + \
+            (self.width / self.GRID_SIZE_X / 2)
+        y_position = ((self.height / self.GRID_SIZE_Y) * y_index) + \
+            (self.height / self.GRID_SIZE_Y / 2)
+
+        sprite.set_position(x_position, y_position)
         self.xo_sprite_list.append(sprite)
 
         self.x_turn = not self.x_turn
@@ -49,3 +67,7 @@ class TikTakToe(arcade.Window):
 
     def __load_sprite(self, filename: str) -> arcade.Sprite:
         return arcade.Sprite(self.__getResourcePath(filename), 0.25)
+
+    def __init_board(self) -> None:
+        self.board = [[0 for x in range(self.GRID_SIZE_X)]
+                      for y in range(self.GRID_SIZE_Y)]
